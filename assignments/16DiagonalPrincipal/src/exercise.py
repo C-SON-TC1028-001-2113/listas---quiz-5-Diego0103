fd91da74a2945d0f11768133f5ea0dedc6d4c3a8
Diego0103 <A01254073@itesm.mx>
2021-10-01 17:35:37 +0000

    version1

--- assignments/16DiagonalPrincipal/src/exercise.py
+++ assignments/16DiagonalPrincipal/src/exercise.py
@@ -1,6 +1,26 @@
+def diagonal(m):
+    diag = []
+    largo = len(m)
+    for i in range(largo):
+        diag.append(m[i][i])
+    return(diag)
 
 def main():
-  
+    reng = int(input())
+    columnas = int(input())
+    if reng==columnas:
+        valores = []
+        matriz = []
+        for i in range(reng):
+            for j in range (columnas):
+                valor= int(input())  
+                valores.append(valor)
+            matriz.append(valores)
+            valores = []
+        print (diagonal(matriz))
+    else:
+        print ("La matriz no es cuadrada")
+
 
 if __name__=='__main__':
     main()
